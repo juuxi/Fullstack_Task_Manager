56caca30ecc503a85f1272baff42ee29b1183831
juuxi <dimashopyrev@mail.ru>
2026-06-27 18:43:17 +0300

    add backend delete method

--- backend/task_manager/tasks/views.py
+++ backend/task_manager/tasks/views.py
@@ -33,3 +33,9 @@ class TaskApi(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.delete()
+
+        return Response(status=status.HTTP_200_OK)
