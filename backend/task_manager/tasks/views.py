aebb1927599a81abea6159435291db10fd4d07d5
juuxi <dimashopyrev@mail.ru>
2026-06-28 13:38:52 +0300

    add put method

--- backend/task_manager/tasks/views.py
+++ backend/task_manager/tasks/views.py
@@ -30,7 +30,15 @@ class TaskApi(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        serializer = TaskSerializer(task, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
